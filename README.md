cb32b65878ba5442fe21260ac8cfb86799a06936
mekhinini <elies.mekhinini@etu.unistra.fr>
2024-01-26 09:28:27 +0100

    :memo: adding template

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ npm install node-schedule
 npm i @discordjs/rest
 npm i ms
 
-
+renommer config.template.js -> config.js pui modifer les champs [ ]<br>
 pour executer : node main
---
+++ config.template.js
@@ -0,0 +1,5 @@
+module.exports = {
+    token: "[your token here]",
+    URL_FIVEM: 'https://top-serveurs.net/gta/[your server id here]',
+    ID_CHANNEL_PLAYERS: '[discord channel id here]',
+}
