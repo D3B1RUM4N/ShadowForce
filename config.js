bf66ae2394a5e49acf14800e180aef8b7fe8652c
mekhinini <elies.mekhinini@etu.unistra.fr>
2024-01-26 09:13:16 +0100

    :wrench: update config files

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "MTA5NzkyNDkwODA5MDEzMDUzMg.G10vXo.ROur67LOF5-PSLmpwbV5XecKOtCV3Fgm2nZuNE",
-    URL_FIVEM: 'https://top-serveurs.net/gta/operation-atalante',
-    ID_CHANNEL_PLAYERS: '1097929478065373237',
+    token: "[your token here]",
+    URL_FIVEM: 'https://top-serveurs.net/gta/[your server id here]',
+    ID_CHANNEL_PLAYERS: '[discord channel id here]',
 }
