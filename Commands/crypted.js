d4765e8c14d15b92105faeb85c0258a49dbcca9e
mekhinini <elies.mekhinini@etu.unistra.fr>
2024-01-29 13:07:14 +0100

    :hammer: modif de /crypted

--- Commands/crypted.js
+++ Commands/crypted.js
@@ -4,49 +4,49 @@ const logs = require('../Functions/log.js')
 module.exports = {
     name: "crypted",
     description: "Ecris un message en anonyme, il serra crypté!",
-    permission : "Aucune",
-    category : "RP",
-    dm : false,
+    permission: "Aucune",
+    category: "RP",
+    dm: false,
     options: [{
         name: "pseudo",
         type: "string",
         description: "Le pseudo à afficher (optionnel)",
         required: true
-    },{
+    }, {
         name: "message",
         type: "string",
         description: "Le message à envoyer ( | pour les retours à la ligne)",
         required: true
-    },{
+    }, {
         name: "channel",
         type: "channel",
         description: "Le channel où envoyer le message (optionnel)",
         required: false
     }],
-    
-    
-    
+
+
+
     async run(client, message, args) {
 
         const replacements = {
-            '|' : '\n',
+            '|': '\n',
             'e': '3',
             'E': '3',
             'a': '4',
             'A': '4',
-            'O' : '0',
-            'o' : '0',
-            'I' : '1',
-            'i' : '1',
-            'L' : '1',
-            'l' : '1'
-            
+            'O': '0',
+            'o': '0',
+            'I': '1',
+            'i': '1',
+            'L': '1',
+            'l': '1'
+
             // Add more replacements as needed
         };
-    
+
 
         let msg = ``;
-        msg += `\`\`\`Mail crypté \nConnection en cours... \`\`\``
+        msg += `\`\`\`Mail crypté \nConnection en cours...\nCrypté en SHA-256 \`\`\``
         msg += `**${args.get("pseudo").value}** : `;
 
         msg += args.get("message").value.replace(/[ea]/gi, match => replacements[match] || match);
@@ -55,8 +55,8 @@ module.exports = {
 
 
         if (!channel) {
-        // Si aucun channel n'est spécifié, vous pouvez spécifier un channel par défaut ici
-        channel = client.channels.cache.get(message.channel.id);
+            // Si aucun channel n'est spécifié, vous pouvez spécifier un channel par défaut ici
+            channel = client.channels.cache.get(message.channel.id);
         }
 
         console.log(channelID);
@@ -68,5 +68,5 @@ module.exports = {
     }
 
 
-    
+
 }
